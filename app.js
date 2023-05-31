c55f4c13dd3a751b6dc22a60c1962c9e9676027e
PunThePancake <71299502+PunThePancake@users.noreply.github.com>
2023-06-01 09:37:13 +1000

    update

--- app.js
+++ app.js
@@ -15,17 +15,33 @@ function mobileNavToggle() {
     var bar = document.getElementById('bars');
     var cross = document.getElementById('xMark');
 
-    mobileNav.style.display = 'flex'
-    bar.style.opacity = '0'
-    bar.style.scale = '0'
-    cross.style.opacity = '1'
-    cross.style.scale = '1'
-    cross.style.rotate = '90deg'
-        setTimeout(() => {
-            mobileNav.style.opacity = '1'
-            document.querySelectorAll('.mobile-btn').forEach(el => {
-                el.style.opacity = 1;
-                el.style.transform = 'translateX(0rem)'
-                });
-        }, "1");
+    if (cross.style.rotate === '90deg') {
+        mobileNav.style.opacity = '0'
+        bar.style.opacity = '1'
+        bar.style.scale = '1'
+        cross.style.opacity = '0'
+        cross.style.scale = '0'
+        cross.style.rotate = '0deg'
+        document.querySelectorAll('.mobile-btn').forEach(el => {
+            el.style.opacity = 0;
+            el.style.transform = 'translateX(-3rem)'
+            });
+            setTimeout(() => {
+                mobileNav.style.display = 'none'
+            }, "700"); 
+    } else {
+        mobileNav.style.display = 'flex'
+        bar.style.opacity = '0'
+        bar.style.scale = '0'
+        cross.style.opacity = '1'
+        cross.style.scale = '1'
+        cross.style.rotate = '90deg'
+            setTimeout(() => {
+                mobileNav.style.opacity = '1'
+                document.querySelectorAll('.mobile-btn').forEach(el => {
+                    el.style.opacity = 1;
+                    el.style.transform = 'translateX(0rem)'
+                    });
+            }, "1");
+    }
 }
