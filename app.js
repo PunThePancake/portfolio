ba456bd898a0a4331ed094812cc64218e813873b
PunThePancake <71299502+PunThePancake@users.noreply.github.com>
2023-05-31 11:47:46 +1000

    update

--- app.js
+++ app.js
@@ -9,3 +9,7 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+function mobileNavToggle() {
+    var mobileNav = document.getElementById('mobileNav');
+}
