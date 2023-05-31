cb049f3a05dc611452b74272443b16de6c59672a
PunThePancake <71299502+PunThePancake@users.noreply.github.com>
2023-05-31 12:09:28 +1000

    update

--- app.js
+++ app.js
@@ -12,4 +12,20 @@ hiddenElements.forEach((el) => observer.observe(el));
 
 function mobileNavToggle() {
     var mobileNav = document.getElementById('mobileNav');
+    var bar = document.getElementById('bars');
+    var cross = document.getElementById('xMark');
+
+    mobileNav.style.display = 'flex'
+    bar.style.opacity = '0'
+    bar.style.scale = '0'
+    cross.style.opacity = '1'
+    cross.style.scale = '1'
+    cross.style.rotate = '90deg'
+        setTimeout(() => {
+            mobileNav.style.opacity = '1'
+            document.querySelectorAll('.mobile-btn').forEach(el => {
+                el.style.opacity = 1;
+                el.style.transform = 'translateX(0rem)'
+                });
+        }, "1");
 }
